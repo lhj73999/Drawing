afa9af187d5384e0dd5f01dff30cd8c1503f379c
lhj73999 <ehgkfdl@naver.com>
2014-09-11 13:43:15 +0900

    Revert "11"
    This reverts commit 9104e81eb00f5670217d44458de73280ab179cd1.

--- sketch.js
+++ sketch.js
@@ -1,10 +1,9 @@
 function setup() {
   createCanvas(640, 480);
-  //0;
-  background(255, 0, 0);
+  background(0);
   stroke(255);
   noFill();
   line(10, 10, 90, 10);
-    rect(20, 20, 100, 100);
-    ellipse(320, 240, 100, 50);
+    rect(20, 20, 80, 50);
+    ellipse(50, 50, 50, 50);
 }
