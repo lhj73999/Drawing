d157e94e4bb84fa9887511a1cb3a255a0041bfbe
lhj73999 <ehgkfdl@naver.com>
2014-09-11 14:32:48 +0900

    aa

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ function setup() {
   createCanvas(640, 480);
   background(255, 167, 167);
   stroke(255);
-  Fill(178, 235, 244);
+  noFill();
   line(10, 10, 90, 10);
     rect(20, 20, 100, 100);
     ellipse(320, 240, 100, 50);
