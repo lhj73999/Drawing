55f422390f895cf95d5d9929322bfac3a14209e7
lhj73999 <ehgkfdl@naver.com>
2014-09-11 13:53:43 +0900

    망할

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 function setup() {
   createCanvas(640, 480);
-  background(0);
+  background(255, 167, 167);
   stroke(255);
   noFill();
   line(10, 10, 90, 10);
-    rect(20, 20, 80, 50);
-    ellipse(50, 50, 50, 50);
+    rect(20, 20, 100, 100);
+    ellipse(320, 240, 100, 50);
 }
