0a3e4ad0e14973ab413ffa8083dbe7733dea9c2c
lhj73999 <ehgkfdl@naver.com>
2014-09-11 14:32:09 +0900

    sss

--- sketch.js
+++ sketch.js
@@ -2,8 +2,8 @@ function setup() {
   createCanvas(640, 480);
   background(255, 167, 167);
   stroke(255);
-  noFill();
+  Fill(178, 235, 244);
   line(10, 10, 90, 10);
     rect(20, 20, 100, 100);
     ellipse(320, 240, 100, 50);
-}
+} 
